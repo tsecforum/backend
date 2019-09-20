f999e60528ad88611e3a1ba92479da08dacbfa3d
tseccodecell <tseccodecell@gmail.com>
2019-09-20 22:06:28 +0530

    'commit'

--- bumblebee/settings.py
+++ bumblebee/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'bbdb',
         'USER': 'bb',
-        'PASSWORD': 'myPass',
+        'PASSWORD': 'mypass',
         'HOST': '127.0.0.1',
         'PORT': '',
     }
--- ngo/views.py
+++ ngo/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import NGO,Event
+from django.core import serializers
+from django.http import HttpResponse
 
 # Create your views here.
 def dashboard(request):
-	return HttpResponse("Hello")
+	
+	events = Event.objects.order_by('-post_date')
+	events_json = serializers.serialize('json',qs)
+
+	return HttpResponse(events_json,content_type='application/json')
