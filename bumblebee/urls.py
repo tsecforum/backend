ce5432fb775490f8f8ec85d481e3ac39af6b8069
tseccodecell <tseccodecell@gmail.com>
2019-09-20 20:31:37 +0530

    'Change'

--- bumblebee/urls.py
+++ bumblebee/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('ngo/', include('ngo.urls')),
-    path('',include('accounts.urls'))
+    path('',include('accounts.urls')),
     path('admin/', admin.site.urls),
 ]
