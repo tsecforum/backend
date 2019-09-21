1bd7247f5361bf13e508f20d7b1aa7db6ed221a2
tseccodecell <tseccodecell@gmail.com>
2019-09-21 07:09:40 +0530

    deploy

--- ngo/urls.py
+++ ngo/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
 	path('',views.dashboard,name='dashboard'),
 	path('volunteering/',views.volunteering,name = 'volunteering'),
 	path('donating/',views.donating,name = 'donating'),
-	path('ret_donations/',views.return_donations,name='return_donations')
+	path('ret_donations/',views.return_donations,name='return_donations'),
+	path('details/',views.show_ngos,name='show_ngos')
 ]
